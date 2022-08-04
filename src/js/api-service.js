49bfb6d130ef8e1ad4d7fba96d25f8c755c5fd62
Kirill Lee <cyril.lee1997@gmail.com>
2022-08-04 19:56:57 +0300

    async/await

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,12 +1,13 @@
 const BASE_URL = 'https://restcountries.com/v2';
 
-function fetchCountries(searchQuery) {
-  return fetch(`${BASE_URL}/name/${searchQuery}`).then(response => {
-    if (!response.ok) {
-      throw new Error();
-    }
-    return response.json();
-  });
+async function fetchCountries(searchQuery) {
+  const response = await fetch(`${BASE_URL}/name/${searchQuery}`);
+
+  if (!response.ok) {
+    throw new Error();
+  }
+
+  return await response.json();
 }
 
 export default { fetchCountries };
